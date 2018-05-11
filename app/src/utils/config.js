415cd465859cf8d283cf96560f2ad0a2a66533f4
Simone Torrisi <storrisi@gmail.com>
2018-05-11 17:22:29 +0200

    Fix config

--- app/src/utils/config.js
+++ app/src/utils/config.js
@@ -1,22 +1,22 @@
 const config = {
-  api: 'http://ec2-52-17-134-184.eu-west-1.compute.amazonaws.com:14000/api',
+  api: '',
   maxNotificationsNum: 4,
   requestTimeout: 240000,
   firebaseDevConfig: {
-    apiKey: 'AIzaSyDuI4yAzVMLwHq0k_WWLtZ7rIyuUKFn2n8',
-    authDomain: 'sidweb-sanbox.firebaseapp.com',
-    databaseURL: 'https://sidweb-sanbox.firebaseio.com',
-    projectId: 'sidweb-sanbox',
-    storageBucket: 'sidweb-sanbox.appspot.com',
-    messagingSenderId: '650146106769'
+    apiKey: '',
+    authDomain: '',
+    databaseURL: '',
+    projectId: '',
+    storageBucket: '',
+    messagingSenderId: ''
   },
   firebaseProdConfig: {
-    apiKey: 'AIzaSyDuI4yAzVMLwHq0k_WWLtZ7rIyuUKFn2n8',
-    authDomain: 'sidweb-sanbox.firebaseapp.com',
-    databaseURL: 'https://sidweb-sanbox.firebaseio.com',
-    projectId: 'sidweb-sanbox',
-    storageBucket: 'sidweb-sanbox.appspot.com',
-    messagingSenderId: '650146106769'
+    apiKey: '',
+    authDomain: '',
+    databaseURL: '',
+    projectId: '',
+    storageBucket: '',
+    messagingSenderId: ''
   }
 };
 
